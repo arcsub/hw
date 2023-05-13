0660d7c72644ebd4d42ef025e841d11ebe18a5ea
arcsub <79924091218@ya.ru>
2023-05-13 09:36:44 +0500

    HW3 is completed
    Signed-off-by: arcsub <79924091218@ya.ru>

--- hw03_frequency_analysis/go.mod
+++ hw03_frequency_analysis/go.mod
@@ -1,8 +1,11 @@
-module github.com/fixme_my_friend/hw03_frequency_analysis
+module github.com/arcsub/hw/hw03_frequency_analysis
 
 go 1.19
 
-require github.com/stretchr/testify v1.7.0
+require (
+	github.com/stretchr/testify v1.7.0
+	golang.org/x/exp v0.0.0-20230510235704-dd950f8aeaea
+)
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,36 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"regexp"
+	"sort"
+
+	"golang.org/x/exp/maps"
+)
+
+var RE = regexp.MustCompile(`\s+`)
+
+func Top10(text string) []string {
+	if len(text) == 0 {
+		return nil
+	}
+
+	split := RE.Split(text, -1)
+
+	words := make(map[string]int)
+
+	for _, el := range split {
+		words[el]++
+	}
+
+	result := maps.Keys(words)
+
+	sort.Slice(result, func(i, j int) bool {
+		if words[result[i]] == words[result[j]] {
+			return result[i] < result[j]
+		}
+
+		return words[result[i]] > words[result[j]]
+	})
+
+	return result[:10]
 }
